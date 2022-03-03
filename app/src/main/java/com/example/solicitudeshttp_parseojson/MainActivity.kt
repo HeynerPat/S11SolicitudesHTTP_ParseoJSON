d9235aa1e5488a284d810fa49ad4da0308b28cfa
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-03-03 11:15:41 -0500

    Comprobar si existe una conexión a Internet

--- app/src/main/java/com/example/solicitudeshttp_parseojson/MainActivity.kt
+++ app/src/main/java/com/example/solicitudeshttp_parseojson/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.solicitudeshttp_parseojson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bValidarRed = findViewById<Button>(R.id.bValidarRed)
+        bValidarRed.setOnClickListener{
+            //Código para validar red
+
+            if(Network.hayRed(this)){
+                Toast.makeText(this,"Si hay Red", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this,"No hay una conexión a internet", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 }
